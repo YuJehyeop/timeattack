a0ea9850f4ce5999909eb45704748fde9935958d
YuJehyeop <90807141+YuJehyeop@users.noreply.github.com>
2021-11-26 19:12:49 +0900

    Delete test10 directory

--- test10/src/main/java/com/sparta/weeklytestspring/repository/ArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.weeklytestspring.repository;
-
-import com.sparta.weeklytestspring.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-}
--- test10/src/main/java/com/sparta/weeklytestspring/repository/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.weeklytestspring.repository;
-
-import com.sparta.weeklytestspring.domain.Article;
-import com.sparta.weeklytestspring.domain.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
